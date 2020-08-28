e45f2f30b12098cff11df8ddba70191c2902a2f3
Madeline Zhang <maddyz@mit.edu>
2020-08-28 00:14:04 -0700

    resume button works and is responsive now

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -1,6 +1,5 @@
 (function($) {
   "use strict"; // Start of use strict
-
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -30,13 +29,56 @@
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
+      
+      // places the resumebutton correctly
+      if ($(document).width() >= 992){
+        $("#resume-button").height("0px")
+        $("#resume-button").css("line-height", "0px")
+        $("#resume-button").height($("#navbarResponsive").height() + "px")
+        $("#resume-button").css("line-height", $("#navbarResponsive").height() + "px")
+      }
     } else {
       $("#mainNav").removeClass("navbar-shrink");
+      // places the resumebutton correctly
+      if ($(document).width() >= 992){
+        $("#resume-button").height("0px")
+        $("#resume-button").css("line-height", "0px")
+        $("#resume-button").height($("#navbarResponsive").height() + "px")
+        $("#resume-button").css("line-height", $("#navbarResponsive").height() + "px")
+      }
+
     }
   };
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
+
+  // fixing the resume-button when it's resized
+  $(document).ready(larg);
+  $(window).resize(larg);
+  // $(window).resize(larg);
+ 
+  function larg(){
+    if ($(document).width() < 992){
+      $("#resume-button").css("border","none")
+      $("#resume-button").height("0px")
+      $("#resume-button").css("line-height", 0 + "px")
+      $("#resume-button").css("padding", 0 + "px")
+      $("#resume-button-li").css("padding-top", "5px")
+      $("#resume-button-li").css("padding-bottom", "5px")
+      $("#resume-button-li").height("30px")
+
+    }else{
+      $("#resume-button").height("0px")
+      $("#resume-button").css("line-height", "0px")
+      $("#resume-button").height($("#navbarResponsive").height() + "px")
+      $("#resume-button").css("line-height", $("#navbarResponsive").height() + "px")
+      $("#resume-button").css("padding", 5 + "px")
+      $("#resume-button").css("border","solid")
+      $("#resume-button-li").css("padding-top", "0px")
+      $("#resume-button-li").css("padding-bottom", "0px")
+    }
+  }
 
 })(jQuery); // End of use strict

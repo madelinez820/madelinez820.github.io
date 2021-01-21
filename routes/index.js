819d2c0782dd3c09a0afd85db1116397ee364ec8
Madeline Zhang <maddyz@mit.edu>
2021-01-21 01:57:41 -0800

    trying to make file path to resume prettier

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/links', function(req, res, next) {
   res.sendFile('links.html', { root: 'views' });
 });
 
+/* GET resume pdf */
+router.get('/resume', function(req, res, next) {
+  res.sendFile('madelinezhangresume.pdf', { root: 'views' });
+});
+
 module.exports = router;
